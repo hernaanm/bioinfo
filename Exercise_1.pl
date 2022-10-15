026381b1f20fad9291993856bb59290b5e06fb13
hernaanm <her.mateika@gmail.com>
2022-10-15 23:04:59 +0200

    exercise 1

---
+++ Exercise_1.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+  
+# Modules used
+use strict;
+use warnings;
+  
+# Print function 
+print("Hello World\n");
+
+use Bio::SeqIO;
+use Bio::SeqUtils;
+
+my $seqio_obj;
+my $seq_obj;
+my $seqout;
+
+my $outputPath = "Output/" ;
+my $exercise = "Exercise_1";
+
+
+$seqio_obj = Bio::SeqIO->new(-file => "sequence.gb", 
+                             -format => "genbank" ); 
+
+$seq_obj = $seqio_obj->next_seq;
+
+
+my @all_trans = Bio::SeqUtils->translate_6frames($seq_obj);
+
+
+	for (my $i = 0; $i < @all_trans; $i++) {
+        print("Sequence_" . ($i + 1) );
+        print("\n");
+	    print($all_trans[$i]->seq);
+        print("\n");
+        print("\n");
+        $seqout = Bio::SeqIO->new(-file => ">" . $outputPath . $exercise . "/output_" . ($i + 1) . ".fasta", -format => "fasta");
+        $seqout->write_seq($all_trans[$i]);
+        
+    }
